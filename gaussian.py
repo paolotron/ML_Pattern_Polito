f79629be5a75425bf9bb143ad18eb2820eae820f
paolotron <s283282@studenti.polito.it>
2021-03-25 20:15:46 +0100

    added Guassian probability functions

---
+++ gaussian.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import ml_p.probability as p
+
+if __name__ == "__main__":
+    gaus = np.load("datasets/XGau.npy")
+    plt.hist(gaus, bins=50, density=True)
+    XPlot = np.linspace(-8, 12, 1000)
+    ll_samples = p.GAU_pdf(XPlot, 0, 2)
+    plt.plot(XPlot, ll_samples)
+    plt.title("normal density vs the data")
+    plt.show()
+
+    print("The likelihood is: ", ll_samples.prod())
+
+    mu = np.mean(gaus)
+    v = np.var(gaus)
+    log_gaus = p.GAU_logpdf(gaus, mu, v)
+    plt.hist(gaus, bins=50, density=True)
+    XPlot = np.linspace(-8, 12, 1000)
+    log_samples = p.GAU_logpdf(XPlot, mu, v)
+    plt.plot(XPlot, np.exp(log_samples))
+    plt.title("normal density with estimation of mu and v vs The Data")
+    plt.show()
+    print(sum(log_gaus))
+
+    XND = np.load("Solutions/XND.npy")
+    mu = np.load("Solutions/muND.npy")
+    C = np.load("Solutions/CND.npy")
+    pdfSol = np.load("Solutions/llND.npy")
+    pdfGau =p.GAU_ND_logpdf(XND, mu, C)
+    print(np.abs(pdfSol - pdfGau).mean())
+
+
---
+++ ml_p/probability.py
@@ -0,0 +1,42 @@
+import numpy as np
+import numpy.linalg as ln
+
+
+def GAU_pdf(x: np.ndarray, mu: float, var: float) -> np.ndarray:
+    """
+    Probability function of Guassian distribution
+    :param x: ndarray input parameters
+    :param mu: float mean of the distribution
+    :param var: float variance of the distribution
+    :return: ndarray probability of each sample
+    """
+    k = (1 / (np.sqrt(2 * np.pi * var)))
+    up = -np.power(x - mu, 2) / (2 * var)
+    return k * np.exp(up)
+
+
+def GAU_logpdf(x: np.ndarray, mu: float, var: float) -> np.ndarray:
+    """
+    Log probability function of Guassian distribution
+    :param x: ndarray input parameters
+    :param mu: float mean of the distribution
+    :param var: float variance of the distribution
+    :return: ndarray log probability of each sample
+    """
+    return -0.5 * np.log(2 * np.pi) - 0.5 * np.log(var) - np.power(x - mu, 2) / (2 * var)
+
+
+def GAU_ND_logpdf(x: np.ndarray, mu: np.ndarray, cov: np.ndarray) -> np.ndarray:
+    """
+    Multivarate Gaussian Distribution probability function<
+    :param x: ndarray input matrix
+    :param mu: ndarray mean vector
+    :param cov: ndarray covariance matrix
+    :return: ndarray
+    """
+    M = x.shape[0]
+    s, ld = ln.slogdet(cov)
+    k = -M * np.log(2 * np.pi) * 0.5 - ld * 0.5
+    f = x - mu
+    res = k - .5 * ((f.T @ ln.inv(cov)).T * f).T.sum(-1)
+    return res
